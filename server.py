1e92fe170c7282705cb1565d5d9ba330fda7bf73
inmorgtal <inmorgtal@gmail.com>
2025-12-30 17:25:17 +0100

    MORGAN INIZIO

--- server.py
+++ server.py
@@ -5,9 +5,17 @@ sSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-socket.bind(("0.0.0.0", 1234))
+socket.bind(("localhost", 1234))
 
 sSocket.listen(4)
+
+listaGiocatori=[]
+
+while True: 
+    cSocket,cAddr = sSocket.accept()
+    listaGiocatori.append(cSocket)
+
+
 
 
 
