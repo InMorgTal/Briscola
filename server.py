ffc78c9de143f35459ec02a14e5d4ebd26fd5fc9
inmorgtal <inmorgtal@gmail.com>
2025-12-30 16:12:30 +0100

    setup

--- server.py
+++ server.py
@@ -1,1 +1,13 @@
 
+import socket
+
+sSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+socket.bind(("0.0.0.0", 1234))
+
+sSocket.listen(4)
+
+
+
